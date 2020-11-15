eb395c4009ec7560d3399f69ed0ab49233422d85
elvis10ten <elvis.chidera@gmail.com>
2020-11-15 15:18:25 -0500

    Polishing 1

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui"
+
+const Footer = () => {
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by Elvis Chidera. All rights reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
