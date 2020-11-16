33ac83310214fe23a7ab177eea768060e2417810
elvis10ten <elvis.chidera@gmail.com>
2020-11-16 14:17:24 -0500

    Polishing 3

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,4 @@ import Prism from "prism-react-renderer/prism";
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 
 require("prismjs/components/prism-kotlin");
+require("prismjs/components/prism-groovy");
